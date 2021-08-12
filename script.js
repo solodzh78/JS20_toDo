996ff6ac38ba7c2f93fbea460aecf061d479a9cb
Sergei Solodzhuk <solodzh@gmail.com>
2021-08-12 03:45:07 +0200

    Задание 12 промежуточное

--- script.js
+++ script.js
@@ -5,21 +5,74 @@ const todoControl = document.querySelector('.todo-control'),
       todoList = document.querySelector('.todo-list'),
       todoCompleted = document.querySelector('.todo-completed');
 
-const todoData = [
-  {
-    value: 'Сварить кофе',
-    completed: false
-  },
-  {
-    value: 'Помыть посуду',
-    completed: true
-  },
-];
+const todoData = [];
 
 const render = function() {
+
+  todoList.textContent = '';
+  todoCompleted.textContent = '';
+
+
+  todoData.forEach(function(item) {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+
+    li.innerHTML = 
+    '<span class="text-todo">' + item.value + '</span>' +
+    '<div class="todo-buttons">' +
+      '<button class="todo-remove"></button>' +
+      '<button class="todo-complete"></button>' +
+		'</div>';
+
+    if (item.completed) {
+      todoCompleted.append(li);
+    } else {
+      todoList.append(li);
+    }
+
+    const btnTodoCompleted = li.querySelector('.todo-complete');
+    const btnTodoRemove = li.querySelector('.todo-remove');
+
+
+    btnTodoCompleted.addEventListener('click', function() {
+      item.completed = !item.completed;
+      render();
+    });
+
+    btnTodoRemove.addEventListener('click', function(e) {
+
+      const obj = {
+        
+        value: e.target.parentNode.parentNode.childNodes[0].textContent,
+        completed: e.target.parentElement.parentElement.parentElement.id === 'todo'
+        ? false : true
+      };
+      console.log('obj: ', obj);
+      console.log('todoData[0]: ', todoData[0]);
+      console.log(todoData[0] === obj);
+      const objIndex = todoData.indexOf(obj);
+      console.log('objIndex: ', objIndex);
+
+      render();
+    });
+
+  });
 
 };
 
 todoControl.addEventListener('submit', function(event) {
   event.preventDefault();
+  if (headerInput.value !== '') {
+    const newTodo = {
+      value: headerInput.value,
+      completed: false,
+    };
+    
+    todoData.push(newTodo);
+    headerInput.value = '';
+    render();
+  }
+
 });
+
+render();
